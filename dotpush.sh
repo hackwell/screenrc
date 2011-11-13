8f04d7a5885c8636fc199dcdb55fc139620e0d22
px <px@ns1.net>
2011-11-12 19:06:49 -0800

    hmm

--- dotpush.sh
+++ dotpush.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-# Filename: dotpush.sh
-# Purpose: pushing the shit out of dot files since 2005
-# Last Update: 2009.08.08
-# Current Author: Rob Friedman / <px@ns1.net> / http://px.ns1.net
-# ===============================================================
-
-# ===============================================================
-# SEE ALSO:
-# http://github.com/px/screenrc/
-# ===============================================================
+echo <<< INFO
+Filename: dotpush.sh
+Purpose: pushing the shit out of dot files since 2005
+Last Update: 2009.08.08
+Current Author: Rob Friedman / <px@ns1.net> / http://px.ns1.net
+===============================================================
+SEE ALSO:
+http://github.com/px/screenrc/
+===============================================================
+INFO
 
 ## 
 echo pushing out . files stuff
